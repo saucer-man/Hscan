90e54c19712bda0cb682749a22893123701f2a91
saucer-man <gyq0929@gmail.com>
2020-04-09 16:23:50 +0800

    add springboot actuator script

---
+++ pocs/actuator.py
@@ -0,0 +1,13 @@
+import requests
+
+# refer https://xz.aliyun.com/t/2233
+
+
+def poc(host, port, timeout):
+    try:
+        res = requests.get(f"http://{host}:{port}", timeout=timeout, verify=False)
+        if"node exporter" in res.text.lower():
+            return "SpringBoot Actuator is unauthorized"
+    except:
+        pass
+    return None
